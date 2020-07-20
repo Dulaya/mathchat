071bc3d1e5d3978a9365c91d50b8839fa0679285
Dulaya Saennok <dulaya.saennok@gmail.com>
2020-07-19 21:23:48 -0700

    Update submit button to trigger when Enter press on Chat.js and minor App.css updates

--- src/Chat.js
+++ src/Chat.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Latex from './components/latex';
 import Distance from './components/distance';
 import Foil from './components/foil';
 import Combination from './components/combination';
@@ -50,6 +49,10 @@ class Bubble extends React.Component{
             <div id="botCover">
               <img id="botImage" src="images/robot.svg" alt="Bot: " /> 
               <div className="bubble botChat" >
+                {
+                //Conditional rendering component based on user input
+                //if (this.topic(value) == "menu") {<Menu />} else {null}
+                }
                 {this.topic(value) == "menu" ? <Menu /> : null}
                 {this.topic(value) == "distance" ? <Distance /> : null}
                 {this.topic(value) == "combination" ? <Combination /> : null}
@@ -57,6 +60,7 @@ class Bubble extends React.Component{
                 {this.topic(value) == "pythagorean" ? <Pythagorean /> : null}
                 {this.topic(value) == "foil" ? <Foil /> : null}
                 {
+                  //if user input does not equal to specific keyword, output menu message
                   this.topic(value) != "menu" && this.topic(value) != "distance" && this.topic(value) != "pythagorean" && 
                   this.topic(value) != "combination" && this.topic(value) != "permutation" && this.topic(value) != "foil" 
                     ? "I'm not smart enough to process natural lanuguage :( Please enter a valid input. E.g. Menu, Distance, Pythagorean..." : null
@@ -80,22 +84,40 @@ class Chat extends React.Component{
     }
   }
 
+  enterPressed = () => {
+  document.getElementById("userInput").addEventListener("keyup", function(event) {
+    event.preventDefault();
+    event.Handled = true;
+    if (event.keyCode === 13) {
+      document.getElementById("submitButton").click();
+    }
+  });
+}
+
   submit = () => { //Submit user input
     var user_input = document.getElementById("userInput").value;
-    //Check if user_input is in this.state.current_chat. If so, replace with new one. 
-    //Do this because multiple components (e.g. distances) does work with many values 
-    if(this.state.current_chat.includes(user_input)==true){
-      const temp_array = this.state.current_chat;
-      const index = temp_array.indexOf(user_input);
-      if (index > -1) {temp_array.splice(index, 1)}
-      this.setState({current_chat:temp_array})
+
+    if(user_input.replace(/\s/g,"") == ""){
+      //Warning for empty input
+      //Also acts as a counter measure for preventing double chatbox
     }
-    this.setState ((prevState) => ({
-      current_chat: [...prevState.current_chat, user_input] //Using spread syntax to append current_chat array
-    }), function(){
-      document.getElementById("userInput").value = '';
-      window.scrollTo(0, document.body.scrollHeight);
-    }) //Set input bar to blank after submit; call back funct due Asynch
+
+    else {
+      //Check if user_input is in this.state.current_chat. If so, replace with new one. 
+      //Do this because multiple components (e.g. distances) does work with many values 
+      if(this.state.current_chat.includes(user_input)==true){
+        const temp_array = this.state.current_chat;
+        const index = temp_array.indexOf(user_input);
+        if (index > -1) {temp_array.splice(index, 1)}
+        this.setState({current_chat:temp_array})
+      }
+      this.setState ((prevState) => ({
+        current_chat: [...prevState.current_chat, user_input] //Using spread syntax to append current_chat array
+      }), function(){
+        document.getElementById("userInput").value = '';
+        window.scrollTo(0, document.body.scrollHeight);
+      }) //Set input bar to blank after submit; call back funct due Asynch
+    }
   }
 
   delete = (id) => { //Delete inividual chat bubble; pass this function down to Bubble component as props
@@ -118,9 +140,10 @@ class Chat extends React.Component{
           delete={this.delete} 
           />
         <div id="inputCover">
-          <input type="text" id="userInput" placeholder="e.g. Menu, Distance, Pythagorean..."/>
+          <input type="text" id="userInput" onKeyDown={this.enterPressed} placeholder="e.g. Menu, Distance, Pythagorean..."/>
           <button id="submitButton" onClick={this.submit}>Enter</button>
         </div>
+        <div id="warning"></div>
       </div>
     )
   }  
